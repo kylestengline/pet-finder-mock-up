95c0494cdb8b3312de55b0d8fbaef76c20f25a2a
Kyle Stengline <kyle.stengline@gmail.com>
2017-03-02 14:37:27 -0800

    updates schema for photos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170302091458) do
+ActiveRecord::Schema.define(version: 20170302223633) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -42,6 +42,7 @@ ActiveRecord::Schema.define(version: 20170302091458) do
     t.string "color"
     t.string "size"
     t.string "birth_date"
+    t.string "photo"
   end
 
 end
