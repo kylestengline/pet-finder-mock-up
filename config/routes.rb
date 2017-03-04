4ab0975397a82288b28850470baaeaf439738d1a
Kyle Stengline <kyle.stengline@gmail.com>
2017-03-03 17:39:39 -0800

    adds homepage mvc, updates header in app html

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :admins
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: 'dogs#index'
+  root to: 'homepages#index'
+
+  resources :dogs, only: [:index, :show]
 
   namespace :admin do
     resources :dogs
