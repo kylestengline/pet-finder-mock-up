e2c74a299045b2db1ac44e31d863bfc8edcce685
Kyle Stengline <kyle.stengline@gmail.com>
2017-03-05 11:41:07 -0800

    seeds data to play with

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,13 +1,12 @@
 class Dog < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  belongs_to :admin
 
   validates :name, presence: true
   validates :age, presence: true
   validates :breed, presence: true
   validates :gender, presence: true
   validates :adoptable, presence: true
-  validates :size_when_grown, presence: true
+  validates :size, presence: true
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+unless Admin.find_by( email: "stuff@things.com" )
+  Admin.create!(
+    email: "stuff@things.com",
+    password: "password"
+  )
+end
+
+Dog.create!([
+  { 
+    name: "Jill" ,
+    age: 2 ,
+    breed: "Corgi" ,
+    gender: "female",
+    adoptable: true,
+    size: "small",
+    photo: "http://3milliondogs.com/blog-assets-two/2014/08/corgicute.jpg",
+    color: "white, black, orange",
+    birth_date: "11/2/2015" 
+  }
+])
+
+p "Created #{Admin.count} Admins"
+p "Created #{Dog.count} Dogs"
