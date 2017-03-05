3b0273cf28c4276ff23dea7b5b65ec4e563fe3a2
Kyle Stengline <kyle.stengline@gmail.com>
2017-03-05 14:36:24 -0800

    implements search form

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,10 +1,15 @@
 class DogsController < ApplicationController
 
   def index
-    
+    @dogs = Dog.all
+    if params[:search]
+      @dogs = Dog.search(params[:search]).order("#{@dogs.age}")
+    else
+      @dogs = Dog.all.order("created_at DESC")
+    end
   end
 
   def show
-    @dogs = Dog.all
+    @dog = Dog.find(params[:id])
   end
 end
--- app/models/dog.rb
+++ app/models/dog.rb
@@ -2,6 +2,10 @@ class Dog < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
 
+  def self.search(search)
+    where("location ILIKE ? OR breed ILIKE ? OR age ILIKE ? OR gender ILIKE ?", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%", "%#{search}%") 
+  end
+
   validates :name, presence: true
   validates :age, presence: true
   validates :breed, presence: true
