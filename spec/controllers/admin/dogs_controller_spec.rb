4654dee49ae7c7af7afff8f67120bce0782eea01
Kyle Stengline <kyle.stengline@gmail.com>
2017-04-05 20:45:10 -0700

    passes create dog controller spec

--- spec/controllers/admin/dogs_controller_spec.rb
+++ spec/controllers/admin/dogs_controller_spec.rb
@@ -11,8 +11,8 @@ RSpec.describe Admin::DogsController, type: :controller do
 
     context "as a signed in admin" do
       it "allows admins to create new dogs" do
-        dog_params = FactoryGirl.attributes_for(:dog) 
-        post :create, dog: dog_params
+        dog_params = FactoryGirl.build(:dog).attributes
+        post :create, params: { dog: dog_params }
         expect(response).to redirect_to admin_dogs_path
       end
     end
