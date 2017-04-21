a377b372c8a990f92ac99aa7360851b245fa1b34
Kyle Stengline <kyle.stengline@gmail.com>
2017-04-20 20:42:10 -0700

    updates feature spec for homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -13,7 +13,7 @@ RSpec.feature "Showing content on the homepage" do
     expect(page).to have_link "Find A Dog To Adopt"
     expect(page).to have_link "About Dog Adoption"
     expect(page).to have_link "Dog Care"
-    expect(page).to have_link "Shelters and Rescues"
+    expect(page).to have_link "Some of Our Pets"
     expect(page).to have_link "Helping Dogs"
 
   end
