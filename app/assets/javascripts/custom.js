12b4bc258f5bcfcac8318ae6064d896f90fa692f
Kyle Stengline <kyle.stengline@gmail.com>
2017-05-06 11:31:58 -0700

    fixes css

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -3,34 +3,24 @@
 $(document).ready(function(){
   console.log("loaded");
 
-  var adoptionLink = $(".page-scroll-adoption");
-  var careLink = $(".page-scroll-care");
-  var helpLink = $(".page-scroll-help");
+  var atags = $(".page-scroll");
   var adoptLi = $(".about-dog-adoption");
   var careLi = $(".dog-care");
   var helpLi = $(".help-dogs");
 
   function dropdown() {
     adoptLi.fadeIn(500);
-    careLi.fadeIn(500);
-    helpLi.fadeIn(500);
+/*    careLi.fadeIn(500);
+    helpLi.fadeIn(500); */
   }
 
   function removeDropdown() {
     adoptLi.fadeOut(500);
-    careLi.fadeOut(500);
-    helpLi.fadeOut(500);
+   /* careLi.fadeOut(500);
+    helpLi.fadeOut(500);*/
   }
 
-  adoptionLink.hover(
-    dropdown, removeDropdown
-  );
-
-  careLink.hover(
-    dropdown, removeDropdown
-  );
-
-  helpLink.hover(
+  atags.hover(
     dropdown, removeDropdown
   );
 
