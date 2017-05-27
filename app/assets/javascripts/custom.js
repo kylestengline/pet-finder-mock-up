e046ef188ce1e612a9fd531c273b2512472d9546
Kyle Stengline <kyle.stengline@gmail.com>
2017-05-26 19:50:51 -0700

    styles dropdown menus

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -8,6 +8,7 @@ $(document).ready(function(){
   var careLi = $(".dog-care");
   var ourDogs = $(".our-pets");
   var helpLi = $(".help-dogs");
+  var navLinks = [ adoptLi, careLi, ourDogs, helpLi ];
 
   dogSearch.hide();
   helpLi.hide();
@@ -17,7 +18,12 @@ $(document).ready(function(){
 
   atags.hover(
     function(){
-      $(this).children('a').addClass("hover");
+      $(this).children('a').addClass("hover")
+      for (var i = 0; i < navLinks.length; i++) {
+        navLinks[i].css("background-color", "black");
+        navLinks[i].css("padding-top", "10px");
+        navLinks[i].css("padding-bottom", "10px");
+      }
       $(this).children('ul').show();
     }, function() {
       $(this).children('a').removeClass("hover");
