aad05bc65256eeb96be12ea13796e76e5b9eb692
Kyle Stengline <kyle.stengline@gmail.com>
2017-05-28 17:18:56 -0700

    styles dropdowns on homepage, changes link name on homepage

--- spec/features/dog_info_pages_spec.rb
+++ spec/features/dog_info_pages_spec.rb
@@ -32,7 +32,7 @@ RSpec.feature "Dog Info Pages" do
   
   scenario "renders the bring a dog home page" do
 
-    click_link "Bring a Dog Home"
+    click_link "Dog Home Care"
 
     expect(page).to have_current_path dog_home_info_path
     expect(page).to have_selector('h1', text: 'More Info about bringing a Dog Home')
