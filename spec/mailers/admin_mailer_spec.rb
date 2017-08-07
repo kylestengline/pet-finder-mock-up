3983343698332c761fc2c72e0e421596d92d7902
Kyle Stengline <kyle.stengline@gmail.com>
2017-08-06 18:56:30 -0700

    removes mailer specs

--- spec/mailers/admin_mailer_spec.rb
+++ spec/mailers/admin_mailer_spec.rb
@@ -1,5 +1,4 @@
 require "rails_helper"
 
 RSpec.describe AdminMailer, type: :mailer do
-  
 end
--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,11 +1,4 @@
 require 'rails_helper'
 
 RSpec.describe Admin, type: :model do
-
-  let(:admin) { Admin.create!(email: "test@testin.com", password: "password") }
-
-  it "sends an email when a dog is created" do
-    expect { admin.create_dog_email(admin) }
-      .to change { ActionMailer::Base.deliveries.count }.by(1)
-  end
 end
