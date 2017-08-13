235d4921c42c7068e02acc114cef2d138174ba2d
Kyle Stengline <kyle.stengline@gmail.com>
2017-08-12 23:32:11 -0700

    updates mailers for prod

--- app/mailers/admin_mailer.rb
+++ app/mailers/admin_mailer.rb
@@ -21,24 +21,24 @@ class AdminMailer < Devise::Mailer
   def welcome_email(admin)
     @admin = admin
     #Production
-    #@login_url = "https://dogseeker.herokuapp.com/sign_in" 
-    @login_url = "http://localhost:3000/admins/sign_in" 
+    @login_url = "https://dogseekers.herokuapp.com/sign_in" 
+    #@login_url = "http://localhost:3000/admins/sign_in" 
     mail(to: @admin.email, subject: "Welcome to Dog Seeker!")
   end
 
   def create_dog_email(dog)
     @dog = dog
     #Production
-    #@admin_dogs_url = "https://dogseeker.herokuapp.com/admin/admins"
-    @admin_dogs_url = "http://localhost:3000/admin/admins" 
+    @admin_dogs_url = "https://dogseekers.herokuapp.com/admin/admins"
+    #@admin_dogs_url = "http://localhost:3000/admin/admins" 
     mail(to: @dog.email, subject: "You just added a new dog for adoption.")
   end
   
   def update_dog_email(dog)
     @dog = dog
     #Production
-    #@admin_dogs_url = "https://dogseeker.herokuapp.com/admin/admins"
-    @admin_dogs_url = "http://localhost:3000/admin/admins" 
+    @admin_dogs_url = "https://dogseekers.herokuapp.com/admin/admins"
+    #@admin_dogs_url = "http://localhost:3000/admin/admins" 
     mail(to: @dog.email, subject: "You just updated one of your dogs.")
   end
 
