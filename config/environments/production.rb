6eee3434becdb812bfb18e2588f4d806ea2a37aa
Kyle Stengline <kyle.stengline@gmail.com>
2017-08-14 20:03:50 -0700

    removes unnecessary lines in prod environment

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -58,8 +58,6 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
   # Action Cable endpoint configuration
-  config.action_cable.url = 'wss://motivates.herokuapp.com/cable'
-  config.action_cable.allowed_request_origins = [ 'https://motivates.herokuapp.com', /http:\/\/motivates.herokuapp.*/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
@@ -84,7 +82,6 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "motivate_#{Rails.env}"
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
